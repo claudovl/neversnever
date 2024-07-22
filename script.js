8dd5e75ff9dbf12b5c9ff526e7b433467b4a1fb8
claudovl <146394183+claudovl@users.noreply.github.com>
2024-07-22 13:14:16 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const text = "Я не придумал, что будет на этом сайте. Так что можете пока послушать музыку.";
+    const typewriterText = document.getElementById('typewriter-text');
+
+    let i = 0;
+
+    function typeWriter() {
+        if (i < text.length) {
+            typewriterText.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typeWriter, 100); // Скорость печати (в миллисекундах)
+        }
+    }
+
+    typeWriter();
+});
