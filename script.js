5b65c2092fa0813f34d67e5f08b96d6071aa7384
claudovl <146394183+claudovl@users.noreply.github.com>
2024-07-22 14:09:27 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const text = "Я не придумал, что будет на этом сайте. Так что можете пока послушать музыку.";
     const typewriterText = document.getElementById('typewriter-text');
-
+    
     let i = 0;
-
+    
     function typeWriter() {
         if (i < text.length) {
             typewriterText.innerHTML += text.charAt(i);
